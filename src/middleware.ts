d1d20ec781ace5d580a81e43d9563f69e2f19608
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2023-02-28 22:40:56 +0100

    Move route protection to middleware using JWT token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import { getToken } from 'next-auth/jwt';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
@@ -15,4 +16,31 @@ export async function middleware(req: NextRequest) {
 			return NextResponse.redirect(data.url);
 		}
 	}
+
+	// !Route protection
+	const session = await getToken({ req });
+	if (!session) {
+		// Write the above using if
+		if (req.nextUrl.pathname === '/') {
+			return NextResponse.redirect(`${req.nextUrl.origin}/product`);
+		}
+
+		if (req.nextUrl.pathname.startsWith('/wishlists')) {
+			return NextResponse.redirect(`${req.nextUrl.origin}/auth/signin`);
+		}
+
+		if (req.nextUrl.pathname === '/plan') {
+			return NextResponse.redirect(`${req.nextUrl.origin}/auth/signin`);
+		}
+
+		if (req.nextUrl.pathname.startsWith('/shared-plans')) {
+			return NextResponse.redirect(`${req.nextUrl.origin}/auth/signin`);
+		}
+
+		if (req.nextUrl.pathname.startsWith('/settings')) {
+			return NextResponse.redirect(`${req.nextUrl.origin}/auth/signin`);
+		}
+	}
+
+	return NextResponse.next();
 }

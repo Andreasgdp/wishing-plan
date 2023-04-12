f9e1baef7a3497e788d13cd9c1a572d2584a3340
Andreasgdp <andreasgdp@gmail.com>
2023-04-12 06:39:49 +0200

    Format code

--- packages/api/babel.config.js
+++ packages/api/babel.config.js
@@ -1,5 +1,5 @@
 // This file is used by Babel to configure the transpilation process
 // Reason: https://dev.to/_gdelgado/pitfalls-when-adding-turborepo-to-your-project-4cel
 module.exports = {
-	presets: ['@babel/preset-react'],
+  presets: ["@babel/preset-react"],
 };

3870bee3cdc260dfde2b64eaa881e5df2ca1a968
Andreas Petersen <andreasgdp@gmail.com>
2023-04-20 16:57:47 +0200

    Do not cache auth checks

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -6,11 +6,12 @@ export default createNextApiHandler({
   router: appRouter,
   createContext,
   responseMeta({ ctx, paths, type, errors }) {
+    const notAuth = paths && paths.every((path) => !path.includes("auth"));
     // checking that no procedures errored
     const allOk = errors.length === 0;
     // checking we're doing a query request
     const isQuery = type === "query";
-    if (ctx?.res && allOk && isQuery) {
+    if (ctx?.res && notAuth && allOk && isQuery) {
       // cache request for 1 day + revalidate once every second
       const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
       return {
